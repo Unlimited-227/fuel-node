870db10424d6d13f1c19cae2f84ec435bbc4cafb
run-node <160773499+run-node@users.noreply.github.com>
2024-06-12 07:24:05 +0800

    Update fuel.sh

--- fuel.sh
+++ fuel.sh
@@ -11,68 +11,70 @@ fi
 SCRIPT_PATH="$HOME/Fuel.sh"
 
 function install_node() {
+    # 安装基本组件
+    sudo apt update
+    sudo apt install -y screen git jq
 
-# 安装基本组件
-sudo apt update
-sudo apt install screen git -y
+    # 安装Rust
+    echo "正在安装Rust..."
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+    source $HOME/.cargo/env
 
-# 安装Rust
-echo "正在安装Rust..."
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-source $HOME/.cargo/env
+    # 安装Fuel服务
+    echo "正在安装Fuel服务..."
+    yes y | curl https://install.fuel.network | sh
+    sleep 5
+    source $HOME/.bashrc
 
-# 安装Fuel服务
-echo "正在安装Fuel服务..."
-yes y | curl https://install.fuel.network | sh
-sleep 5
-source /root/.bashrc
+    # 生成P2P密钥
+    source $HOME/.bashrc
+    export PATH=$HOME/.fuelup/bin:$PATH
+    echo "正在生成P2P密钥..."
+    KEY_OUTPUT=$(fuel-core-keygen new --key-type peering)
+    echo "${KEY_OUTPUT}"
+    SECRET=$(echo $KEY_OUTPUT | jq -r '.secret')
+    echo "${SECRET}"
+    
+    # 创建文件夹路径，确保中间路径存在
+    DIR_PATH="$HOME/root/~/"
+    mkdir -p "$DIR_PATH"
 
-# 生成P2P密钥
-source /root/.bashrc
-export PATH=$HOME/.fuelup/bin:$PATH
-echo "正在生成P2P密钥..."
-KEY_OUTPUT=$(fuel-core-keygen new --key-type peering)
-echo "${KEY_OUTPUT}"
-SECRET=$(echo $KEY_OUTPUT | jq -r '.secret')
-echo "${SECRET}"
-# 创建文件夹路径，确保中间路径存在
-DIR_PATH="$HOME/root/~/"
-mkdir -p "$DIR_PATH"
+    # 创建文件路径
+    FILE_PATH="$DIR_PATH/key.txt"
 
-# 创建文件路径
-FILE_PATH="$DIR_PATH/key.txt"
+    # 将 SECRET 写入文件中
+    echo $SECRET > $FILE_PATH
 
-# 将 SECRET 写入文件中
-echo $SECRET > $FILE_PATH
+    # 克隆chain information
+    if [ -d "chain-configuration" ]; then
+        echo "chain-configuration 目录已存在，跳过克隆。"
+    else
+        git clone https://github.com/FuelLabs/chain-configuration.git
+    fi
 
-# 克隆chain information
-git clone https://github.com/FuelLabs/chain-configuration.git
+    # 用户输入节点名称和RPC地址
+    read -p "请输入您的ETH Sepolia RPC地址: " RPC
 
-# 用户输入节点名称和RPC地址
-read -p "请输入您的ETH Sepolia RPC地址: " RPC
+    # 开始配置并运行节点
+    echo "开始配置并启动您的fuel节点..."
 
-# 开始配置并运行节点
-echo "开始配置并启动您的fuel节点..."
+    screen -dmS Fuel bash -c "source /root/.bashrc; fuel-core run \
+    --service-name=fuel-sepolia-testnet-node \
+    --keypair ${SECRET} \
+    --relayer ${RPC} \
+    --ip=0.0.0.0 --port=4000 --peering-port=30333 \
+    --db-path=~/.fuel-sepolia-testnet \
+    --snapshot ~/chain-configuration/ignition \
+    --utxo-validation --poa-instant false --enable-p2p \
+    --reserved-nodes /dns4/p2p-testnet.fuel.network/tcp/30333/p2p/16Uiu2HAmDxoChB7AheKNvCVpD4PHJwuDGn8rifMBEHmEynGHvHrf \
+    --sync-header-batch-size 100 \
+    --enable-relayer \
+    --relayer-v2-listening-contracts=0x01855B78C1f8868DE70e84507ec735983bf262dA \
+    --relayer-da-deploy-height=5827607 \
+    --relayer-log-page-size=500 \
+    --sync-block-stream-buffer-size 30"
 
-screen -dmS Fuel bash -c "source /root/.bashrc; fuel-core run \
---service-name=fuel-sepolia-testnet-node \
---keypair ${SECRET} \
---relayer ${RPC} \
---ip=0.0.0.0 --port=4000 --peering-port=30333 \
---db-path=~/.fuel-sepolia-testnet \
---snapshot ~/chain-configuration/ignition \
---utxo-validation --poa-instant false --enable-p2p \
---reserved-nodes /dns4/p2p-testnet.fuel.network/tcp/30333/p2p/16Uiu2HAmDxoChB7AheKNvCVpD4PHJwuDGn8rifMBEHmEynGHvHrf \
---sync-header-batch-size 100 \
---enable-relayer \
---relayer-v2-listening-contracts=0x01855B78C1f8868DE70e84507ec735983bf262dA \
---relayer-da-deploy-height=5827607 \
---relayer-log-page-size=500 \
---sync-block-stream-buffer-size 30
-"
-
-echo "节点配置完成并尝试启动。请使用screen -r Fuel 以确认节点状态。"
-screen -r Fuel
+    echo "节点配置完成并尝试启动。请使用screen -r Fuel 以确认节点状态。"
 }
 
 function check_service_status() {
@@ -82,7 +84,10 @@ function check_service_status() {
 function backup() {
     mkdir -p $HOME/root/fuel_key
     cp $HOME/root/~/key.txt $HOME/root/fuel_key/
+}
 
+function uninstall() {
+    echo "卸载节点功能尚未实现。"
 }
 
 # 主菜单
